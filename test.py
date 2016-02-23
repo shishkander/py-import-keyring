f2375db22f94e9057955d7d229ad4783921c7cd5
Andrii Shyshkalov <tandrii@chromium.org>
2016-02-23 20:24:19 +0100

    Fix thanks to agable@.

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ import subprocess
 
 env = os.environ.copy()
 k = 'DBUS_SESSION_BUS_ADDRESS'
+py = 'import time as t; s=t.time(); import keyring; print "-> %.3f seconds" % (t.time()-s)'
 if k in env:
     print 'with default env %s=%s' % (k, env[k])
-    py = 'import time as t; s=t.time(); import keyring; print "-> %.3f seconds" % (t.time()-s)'
     subprocess.call(['python', '-c', py])
     env.pop(k)
 print 'without %s in env' % k
